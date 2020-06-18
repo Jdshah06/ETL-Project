68bfd701e24662862f7c6c9ab76be2ac1e3d50e3
bviollier <benoit.viollier.usa@gmail.com>
2020-06-17 17:19:36 -0700

    last

--- ETL_code.sql
+++ ETL_code.sql
@@ -3,7 +3,7 @@ DROP TABLE confirmed;
 DROP TABLE population;
 
 CREATE TABLE airport (
-	airport VARCHAR,
+	airport_name VARCHAR,
 	state VARCHAR,
 	passenger_count INT,
 	year INT
@@ -26,12 +26,21 @@ SELECT * FROM airport;
 SELECT * FROM confirmed;
 SELECT * FROM population;
 
+/* This is the code that works*/
 SELECT p.state, p.population, c.deaths, c.confirmed, p.population_density
 FROM population AS p
 JOIN confirmed AS c
 ON p.state = c.state
 ORDER BY state ASC;
 
+/*This is the code that does not work as there are more than 1 of the same state in df airport*/
+SELECT p.state, p.population, c.deaths, c.confirmed, p.population_density, a.airport, a.passenger_count
+FROM population AS p
+JOIN confirmed AS c
+ON p.state = c.state
+JOIN airport AS a
+ON p.state = a.state
+ORDER BY state ASC;
 
 
 
